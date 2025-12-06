c4a2dd6b0d54fec7f3fb8833c64f0d333bc294c5
6311236 <6311236@edu.vaniercollege.qc.ca>
2025-12-06 17:03:41 -0500

    Update the Address class with isPostalCodeValid helper method

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -15,4 +15,25 @@ public class Address {
     private String city;
     private Province province;
     private String postalCode;
+
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            char c = postalCode.charAt(i);
+            if (i % 2 == 0) {
+                if (!Character.isLetter(c)) {
+                    return false;
+                }
+            }
+            else {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
