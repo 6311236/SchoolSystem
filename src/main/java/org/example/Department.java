b90cafb178e93b5c83ff67f60c8c6f31ced3689f
6311236 <6311236@edu.vaniercollege.qc.ca>
2025-12-08 18:41:11 -0500

    Update the Department class with data fields and missing lombok method

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -2,10 +2,14 @@ package org.example;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @EqualsAndHashCode
 @Getter
 public class Department {
+    private String departmentId;
+    @Setter private String departmentName;
+    private static int nextId = 1;
 }
