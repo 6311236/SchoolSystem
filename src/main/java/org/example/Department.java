a8b9834a093429e95d48b1d735443e0f269d74b1
6311236 <6311236@edu.vaniercollege.qc.ca>
2025-12-08 19:19:06 -0500

    Update the Department class with isDepartmentNameValid method

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -12,4 +12,24 @@ public class Department {
     private String departmentId;
     @Setter private String departmentName;
     private static int nextId = 1;
+
+    /**
+     * checks if a department name is valid or not
+     * @param departmentName the department name to check
+     * @return true if the department name has only letters or spaces and if not false
+     */
+    public static boolean isDepartmentValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
