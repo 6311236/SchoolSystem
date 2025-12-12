8945419a5651a8387d91495ff0188033eefa6fa9
6311236 <6311236@edu.vaniercollege.qc.ca>
2025-12-11 23:54:10 -0500

    Update the Student class with constructor including studentName, gender, address and department

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -4,6 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @EqualsAndHashCode
 @Getter
 @Setter
@@ -18,5 +20,21 @@ public class Student {
 
     public enum Gender {
         MALE, FEMALE
+    }
+
+    /**
+     * make a student with name, gender, address and department given
+     * @param studentName the name of the student
+     * @param gender the gender of the student
+     * @param address the address of the student
+     * @param department the department of the student
+     */
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = Util.toTitleCase(studentName);
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>();
     }
 }
