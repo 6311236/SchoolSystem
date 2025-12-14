49c08c7fd42b594a94241730f3fb33130d19cf61
6311236 <6311236@edu.vaniercollege.qc.ca>
2025-12-14 01:32:28 -0500

    Update the Course class with lombok methods and data fields

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> registeredStudents;
+    private static int nextId = 1;
 }
